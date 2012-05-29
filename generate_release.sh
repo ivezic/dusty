8961fb6cdfb178fe370076d7428d6360e8a09e85
Frank Heymann <frank.heymann@gmail.com>
2012-05-29 14:26:59 +0000

    updated generate release
    git-svn-id: svn+ssh://newton.pa.uky.edu/home67/svn/dusty/trunk@137 0322ba24-8a25-0410-b1d1-b0cd50d0602d

--- generate_release.sh
+++ generate_release.sh
@@ -17,11 +17,17 @@ cat source/solve_matrix.f90 >> release/dusty/dusty.f90
 cat source/winds.f90 >> release/dusty/dusty.f90 
 
 mkdir release/dusty/docs
+cp docs/manual.05.2012.pdf release/dusty/docs/manual.pdf
 cp data release/dusty/ -rf
-cp examples release/dusty/ -rf
 
 echo 'all:' > release/dusty/Makefile
 echo '\t gfortran -O3 -lgmp -fopenmp -o dusty dusty.f90' >> release/dusty/Makefile
 echo 'gfortran -O3 -lgomp -fopenmp -o dusty.exe dusty.f90' > release/dusty/compile.bat
 
-tar -cf release/dusty.tar release/dusty
+
+cd release 
+cd dusty
+tar -xzf ../../examples_release.tar
+cd ..
+tar -cf dusty.tar dusty
+cd ..
